916b710c1902818da6ab727e51775adaa1998860
kycnexynpuwejl <kycnexynpuwejl@protonmail.com>
2022-05-03 20:03:32 +0300

    Add project files.

---
+++ ShopOnline.Api/Repositories/Contracts/IShoppingCartRepository.cs
@@ -0,0 +1,18 @@
+﻿using ShopOnline.Api.Entities;
+using ShopOnline.Models.Dtos;
+
+namespace ShopOnline.Api.Repositories.Contracts
+{
+    public interface IShoppingCartRepository
+    {
+        Task<CartItem> AddItem(CartItemToAddDto cartItemAddDto);
+
+        Task<CartItem> UpdateQty(int id, CartItemQtyUpdateDto cartItemQtyUpdateDto);
+
+        Task<CartItem> DeleteItem(int id);
+
+        Task<CartItem> GetItem(int id);
+
+        Task<IEnumerable<CartItem>> GetItems(int userId);
+    }
+}
---
+++ ShopOnline.Api/Repositories/ProductRepository.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopOnline.Api.Data;
+using ShopOnline.Api.Entities;
+using ShopOnline.Api.Repositories.Contracts;
+
+namespace ShopOnline.Api.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ShopOnlineDbContext shopOnlineDbContext;
+
+        public ProductRepository(ShopOnlineDbContext shopOnlineDbContext)
+        {
+            this.shopOnlineDbContext = shopOnlineDbContext;
+        }
+        async Task<IEnumerable<ProductCategory>> IProductRepository.GetCategories()
+        {
+            var categories = await this.shopOnlineDbContext.ProductCategories.ToListAsync();
+            return categories;
+        }
+
+        async Task<ProductCategory> IProductRepository.GetCategory(int id)
+        {
+            var category = await this.shopOnlineDbContext.ProductCategories.SingleOrDefaultAsync(c => c.Id == id);
+            return category;
+        }
+
+        async Task<Product> IProductRepository.GetItem(int id)
+        {
+            var product = await this.shopOnlineDbContext.Products.FindAsync(id);
+            return product;
+        }
+
+        async Task<IEnumerable<Product>> IProductRepository.GetItems()
+        {
+            var products = await this.shopOnlineDbContext.Products.ToListAsync();
+            return products;
+        }
+    }
+}
